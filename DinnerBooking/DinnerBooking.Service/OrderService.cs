4c7c3aedf9aa2691a2d555b6abf543e6fc250bb4
Unknown <joseph351619@gmail.com>
2019-07-21 17:52:04 +0800

    Set Ajax page data in Order service
    Contains categories and Cuisines in Categories

---
+++ DinnerBooking/DinnerBooking.Service/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinnerBooking.Data.Entities;
+using DinnerBooking.Repository;
+
+namespace DinnerBooking.Service
+{
+    public class OrderService
+    {
+        private readonly CategoryRepository _categoryRepository;
+        private readonly CuisineRepository _orderRepository;
+        public OrderService(DbContext context)
+        {
+            _orderRepository = new CuisineRepository(context);
+            _categoryRepository = new CategoryRepository(context);
+        }
+
+        public List<Category> GetCategories()
+        {
+            return _categoryRepository.ReadAll().ToList();
+        }
+        public List<Cuisine> GetCuisinesInCategory(int categoryId)
+        {
+            return _orderRepository.ReadAll().Where(a => a.CategoryId == categoryId).ToList();
+        }
+
+    }
+}
