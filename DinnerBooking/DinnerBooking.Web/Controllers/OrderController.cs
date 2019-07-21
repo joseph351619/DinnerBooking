518207a8d765c5bff334d944a5a3fdcec5f92c3c
Unknown <joseph351619@gmail.com>
2019-07-21 22:12:57 +0800

    Modify  add css, few javascript and Api to produce tab effect

---
+++ DinnerBooking/DinnerBooking.Web/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using DinnerBooking.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DinnerBooking.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly OrderService _orderService = new OrderService(new DinnerBooking.Data.BaseEntities());
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View(_orderService.GetCategories());
+        }
+        public ActionResult OrderPage(int categoryId)
+        {
+            return PartialView("_OrderPage", _orderService.GetCuisinesInCategory(categoryId));
+        }
+    }
+}
