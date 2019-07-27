66e62f6568fa1601fca3d071c42ebf644479f1ff
Unknown <joseph351619@gmail.com>
2019-07-28 00:09:23 +0800

    Modify partial view logic and add sending mail function

--- DinnerBooking/DinnerBooking.Data/Entities/Booking.cs
+++ DinnerBooking/DinnerBooking.Data/Entities/Booking.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DinnerBooking.Data.Entities
 {
@@ -18,5 +14,21 @@ namespace DinnerBooking.Data.Entities
         public string Phone { get; set; }
         public string Address { get; set; }
         public bool MailSent { get; set; }
+        [NotMapped]
+        public string BookingContent { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Clear();
+            builder.Append($"親愛的 {Name}\r\n\r\n");
+            builder.Append($"我地收到你既訂單喇！\r\n\r\n");
+            builder.Append($"送貨地址：{Address}\r\n");
+            builder.Append($"聯絡電話：{Phone}\r\n\r\n");
+            builder.Append($"貨品清單:\r\n\r\n");
+            builder.Append($"{BookingContent}");
+            builder.Append($"\r\n多謝幫襯！ - 每日晚餐敬上");
+            return builder.ToString();
+        }
     }
 }
